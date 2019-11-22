2100d8753a1f26950c7c8f71a285d39759500b0d
lens <lens.huygh@gmail.com>
2019-11-22 23:53:12 +0100

    add a message with its own restcontroller thats refreshscoped

---
+++ src/main/java/com/example/reservationservice/MessageRestController.java
@@ -0,0 +1,19 @@
+package com.example.reservationservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestController {
+
+    @Value("${message")
+    private String msg;
+
+    @RequestMapping("/message")
+    String message(){
+        return this.msg;
+    }
+}
