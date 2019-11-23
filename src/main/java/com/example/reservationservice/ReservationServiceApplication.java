306e48e4cdf3b8cfdf8d9661faa6c6b7c0ed71c0
lens <lens.huygh@gmail.com>
2019-11-23 20:04:29 +0100

    add eureka dependency add enableDiscoveryClient to application

--- src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.reservationservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
